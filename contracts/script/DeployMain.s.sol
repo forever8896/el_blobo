17c4d4b8f222ff84605cc8258e98d7393f49eb4c
gil7788 <gil7788@gmail.com>
2025-11-23 00:40:48 +0200

    Fix Deployment Script

--- contracts/script/DeployMain.s.sol
+++ contracts/script/DeployMain.s.sol
@@ -6,6 +6,7 @@ import "forge-std/Script.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { ProjectRegistry } from "../src/ProjectRegistry.sol";
 import { RewardVault } from "../src/RewardVault.sol";
+import { IRewardVault } from "../src/IRewardVault.sol";
 import { Main } from "../src/Main.sol";
 
 contract DeployMain is Script {
@@ -14,11 +15,11 @@ contract DeployMain is Script {
         // Load env vars from .env
         // Foundry auto-loads .env at project root.
         // ---------------------------------------------------------------------
-        uint256 deployerPk = vm.envUint("DEPLOYER_PRIVATE_KEY");
-        address deployer = vm.envAddress("DEPLOYER_ADDRESS");
-        address registryOwner = vm.envAddress("REGISTRY_OWNER");
-        address vaultOwner = vm.envAddress("VAULT_OWNER");
-        address usdc = vm.envAddress("USDC_ADDRESS");
+        uint256 deployerPk        = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        address deployer          = vm.envAddress("DEPLOYER_ADDRESS");
+        address registryOwner     = vm.envAddress("REGISTRY_OWNER");
+        address vaultOwner        = vm.envAddress("VAULT_OWNER");
+        address usdc              = vm.envAddress("USDC_ADDRESS");
         uint256 registrationPrice = vm.envUint("REGISTRATION_PRICE");
 
         vm.startBroadcast(deployerPk);
@@ -26,11 +27,10 @@ contract DeployMain is Script {
         // ---------------------------------------------------------------------
         // 1. Deploy ProjectRegistry
         // ---------------------------------------------------------------------
-        // If ProjectRegistry has constructor args, adjust this line.
-        // Owner will be msg.sender (the deployer) inside the constructor.
+        // If ProjectRegistry has different constructor args, adjust here.
         ProjectRegistry registry = new ProjectRegistry(registryOwner);
 
-        // (Optional sanity check: registryOwner is expected to equal deployer)
+        // Optional sanity check
         require(registryOwner == deployer, "REGISTRY_OWNER != DEPLOYER_ADDRESS");
 
         // ---------------------------------------------------------------------
@@ -38,7 +38,6 @@ contract DeployMain is Script {
         // ---------------------------------------------------------------------
         // RewardVault constructor:
         //   constructor(ERC20 _asset, address _vaultOwner, uint256 _registrationPrice)
-        //
         RewardVault vault = new RewardVault(
             ERC20(usdc),
             vaultOwner,
@@ -49,9 +48,10 @@ contract DeployMain is Script {
         // 3. Deploy Main
         // ---------------------------------------------------------------------
         // Main constructor:
-        //   constructor(ProjectRegistry _projectRegistry, RewardVault _vault)
+        //   constructor(ProjectRegistry _projectRegistry, IRewardVault _vault)
         //
-        Main main = new Main(registry, vault);
+        // We cast the RewardVault to IRewardVault explicitly:
+        Main main = new Main(registry, IRewardVault(address(vault)));
 
         // ---------------------------------------------------------------------
         // 4. Log addresses to console
